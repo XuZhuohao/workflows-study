ba4a43a0bf2a0c6970a4dc62549803d159600c96
yui <786725551@qq.com>
2021-03-26 14:27:54 +0800

    init

---
+++ src/main/java/com/yui/study/workflows/WorkflowsApplication.java
@@ -0,0 +1,13 @@
+package com.yui.study.workflows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowsApplication.class, args);
+    }
+
+}
